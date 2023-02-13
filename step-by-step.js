408d0462ca49c74a0945247ef18548ed5cf7b39f
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-12 20:28:58 -0500

    laying architecture of project. Worked on getting server.js up and going

---
+++ step-by-step.js
@@ -0,0 +1,5 @@
+//TODO: Go through class activitites and pull out steps one by one://
+
+//*Dependencies, initialize express, routes, db, npm, ports, listening for ports.//
+
+//? This is to see if I can more or less redo what tabnine is doing, so that I understand better. */
